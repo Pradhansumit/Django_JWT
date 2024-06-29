0354dc618983eaeb02148d6609acb54f75c9aa9d
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-06-29 16:00:33 +0530

    basic token authentication done and created new api app for api functions

--- jwt_project/jwt_project/settings.py
+++ jwt_project/jwt_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wl=t2z1exe2qt7cq)i$2(#(04p8ui5xh#4zn8(46f1tmy3+cx('
+
+JWT_SECRET_KEY = '10wNOHlIvhYKHOTitCKc3ntPt8XRQpx1ZUo0qVhy93rfZV8pJII7oUGwrhAvoR'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +41,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
-
+    'rest_framework',
+    'rest_framework_simplejwt'
+    'api'
 ]
 
+SIMLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
+    "AUTH_HEADER_TYPES": ("JWT"),
+    "AUTH_HEADER_NAME": "HTTP_AUTHENTICATION",
+
+    "SIGNING_KEY": JWT_SECRET_KEY,
+}
+
+# for the authentication of the token the header will be looking for Authentication Header and the value for this.
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
